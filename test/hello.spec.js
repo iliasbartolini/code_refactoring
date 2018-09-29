324127b43e112aaf1ea8542022704740513f15bc
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-09-29 22:42:12 +0200

    Step 3 - Greets you too

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -2,9 +2,21 @@ import hello from "../app/hello";
 import {expect} from "chai";
 
 describe("Hello", function() {
-  it("greets you", function() {
+
+  it("greets the world", function() {
+    //put your name here
     var actual  = hello();
 
     expect(actual).to.equal("Hello, World");
   });
+
+  it("greets you", function() {
+    //put your name here
+    var myName = "Ilias";
+
+    var actual  = hello(myName);
+
+    expect(actual).to.equal(`Hello, ${myName}`);
+  });
+  
 });
