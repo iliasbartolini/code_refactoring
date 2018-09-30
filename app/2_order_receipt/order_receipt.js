0f96f15de9633077a092432af65e3622b10dba60
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-09-30 22:04:07 +0200

    Refactoring order_receipt: rename variable

--- app/2_order_receipt/order_receipt.js
+++ app/2_order_receipt/order_receipt.js
@@ -7,21 +7,21 @@ export default class OrderReceipt {
 
     printReceipt() {
 
-        let output = this.header();
+        let receipt = this.header();
 
         // prints lineItems
         let totSalesTx = 0.0;
         let tot = 0.0;
 
         this._o.lineItems().forEach((lineItem) => {
-            output += lineItem.description();
-            output += '\t';
-            output += lineItem.price();
-            output += '\t';
-            output += lineItem.quantity();
-            output += '\t';
-            output += lineItem.totalAmount();
-            output += '\n';
+            receipt += lineItem.description();
+            receipt += '\t';
+            receipt += lineItem.price();
+            receipt += '\t';
+            receipt += lineItem.quantity();
+            receipt += '\t';
+            receipt += lineItem.totalAmount();
+            receipt += '\n';
 
             // calculate sales tax @ rate of 10%
             var salesTax = lineItem.totalAmount() * .10;
@@ -32,11 +32,11 @@ export default class OrderReceipt {
         });
 
         // prints the state tax
-        output += "Sales Tax" + '\t' + totSalesTx;
+        receipt += "Sales Tax" + '\t' + totSalesTx;
 
         // print total amount
-        output += "Total Amount" + '\t' + tot;
-        return output.toString();
+        receipt += "Total Amount" + '\t' + tot;
+        return receipt.toString();
     }
 
 
