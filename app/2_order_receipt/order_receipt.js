37ad7773880d86bc69d00a3da92f9e3d6d8033c4
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-09-30 22:28:16 +0200

    Refactoring order_receipt: Feature envy - extract methods to be moved

--- app/2_order_receipt/order_receipt.js
+++ app/2_order_receipt/order_receipt.js
@@ -13,22 +13,35 @@ export default class OrderReceipt {
         let totalSaleTax = 0.0;
         let totalAmount = 0.0;
 
-        this._o.lineItems().forEach((lineItem) => {
-            receipt += lineItem.description();
-            receipt += "\t";
-            receipt += lineItem.price();
-            receipt += "\t";
-            receipt += lineItem.quantity();
-            receipt += "\t";
-            receipt += lineItem.lineAmount();
-            receipt += "\n";
-
-            // calculate sales tax @ rate of 10%
+        function saleTax(lineItem) {
+// calculate sales tax @ rate of 10%
             var salesTax = lineItem.lineAmount() * .10;
-            totalSaleTax += salesTax;
+            return salesTax;
+        }
+
+        function totalLineAmount(lineItem) {
+            return lineItem.lineAmount() + saleTax(lineItem);
+        }
+
+        function lineItemReceipt(lineItem) {
+            let receiptLine = lineItem.description();
+            receiptLine += "\t";
+            receiptLine += lineItem.price();
+            receiptLine += "\t";
+            receiptLine += lineItem.quantity();
+            receiptLine += "\t";
+            receiptLine += lineItem.lineAmount();
+            receiptLine += "\n";
+            return receiptLine
+        }
+
+        this._o.lineItems().forEach((lineItem) => {
+            receipt += lineItemReceipt(lineItem);
+
+            totalSaleTax += saleTax(lineItem);
 
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            totalAmount += lineItem.lineAmount() + salesTax;
+            totalAmount += totalLineAmount(lineItem);
         });
         receipt += this.footer(totalSaleTax, totalAmount);
         return receipt;
