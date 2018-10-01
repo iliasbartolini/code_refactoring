b0edd1c1fcadb8fe966eb5025fe69aadfee79fd3
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-10-01 03:02:35 +0200

    Refactoring direction: - Replace switch with lookup tables

--- app/4_direction/direction.js
+++ app/4_direction/direction.js
@@ -6,32 +6,27 @@ export default class Direction {
 
 
     turnRight() {
-        switch (this._direction) {
-            case 'N':
-                return new Direction('E');
-            case 'S':
-                return new Direction('W');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw 'Invalid direction';
-        }
+        const newDirection = Direction._turnRightLookup[this._direction];
+        return new Direction(newDirection);
     }
 
     turnLeft() {
-        switch (this._direction) {
-            case 'N':
-                return new Direction('W');
-            case 'S':
-                return new Direction('E');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw 'Invalid direction';
-        }
+        const newDirection = Direction._turnLeftLookup[this._direction];
+        return new Direction(newDirection);
     }
+
 }
+
+Direction._turnLeftLookup = {
+    "N": "W",
+    "E": "N",
+    "S": "E",
+    "W": "S"
+};
+
+Direction._turnRightLookup = {
+    "N": "E",
+    "E": "S",
+    "S": "W",
+    "W": "N"
+};
