f911b00872606a7e429e0947c2de9db669dc35b3
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-10-01 03:44:55 +0200

    Refactoring rover: Long method - Extract methods

--- app/5_rover/rover.js
+++ app/5_rover/rover.js
@@ -14,54 +14,15 @@ export default class Rover {
             let command = instructions.charAt(i);
 
             if(command === "M") {
-                switch (this._direction) {
-                    case "N":
-                        this._y++;
-                        break;
-                    case "S":
-                        this._y--;
-                        break;
-                    case "E":
-                        this._x++;
-                        break;
-                    case "W":
-                        this._x--;
-                        break;
-                }
+                this.move();
             }
 
             if(command === "L") {
-                switch (this._direction) {
-                    case "N":
-                        this._direction = "W";
-                        break;
-                    case "S":
-                        this._direction = "E";
-                        break;
-                    case "E":
-                        this._direction = "N";
-                        break;
-                    case "W":
-                        this._direction = "S";
-                        break;
-                }
+                this.turnLeft();
             }
 
             if(command === "R") {
-                switch (this._direction) {
-                    case "N":
-                        this._direction = "E";
-                        break;
-                    case "S":
-                        this._direction = "W";
-                        break;
-                    case "E":
-                        this._direction = "N";
-                        break;
-                    case "W":
-                        this._direction = "S";
-                        break;
-                }
+                this.turnRight();
             }
         }
 
@@ -69,4 +30,54 @@ export default class Rover {
 
     }
 
+    turnRight() {
+        switch (this._direction) {
+            case "N":
+                this._direction = "E";
+                break;
+            case "S":
+                this._direction = "W";
+                break;
+            case "E":
+                this._direction = "N";
+                break;
+            case "W":
+                this._direction = "S";
+                break;
+        }
+    }
+
+    turnLeft() {
+        switch (this._direction) {
+            case "N":
+                this._direction = "W";
+                break;
+            case "S":
+                this._direction = "E";
+                break;
+            case "E":
+                this._direction = "N";
+                break;
+            case "W":
+                this._direction = "S";
+                break;
+        }
+    }
+
+    move() {
+        switch (this._direction) {
+            case "N":
+                this._y++;
+                break;
+            case "S":
+                this._y--;
+                break;
+            case "E":
+                this._x++;
+                break;
+            case "W":
+                this._x--;
+                break;
+        }
+    }
 }
